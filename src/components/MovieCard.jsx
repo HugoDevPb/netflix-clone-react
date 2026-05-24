b45255a5b77e9ed73a7b6e8586f2f14890d566e6
HugoDevPb <hugolinsp18@gmail.com>
2026-05-24 16:49:53 -0300

    Create MovieCard.jsx

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,10 @@
+function MovieCard({ title, image }) {
+  return (
+    <div className="card">
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+    </div>
+  )
+}
+
+export default MovieCard
